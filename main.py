7be525c4bf1ebbe5ada3ad1442ef93862d16000f
kaira <justaidajam@gmail.com>
2025-06-18 23:33:43 +0100

    #

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def main():
 
     logger = logging.getLogger(__name__)
     logger.info("Application starting...")
+    logger.info("v.1.94")
 
     broadcaster = config.get("broadcaster", {}).get("api_base_url")
     if broadcaster:
